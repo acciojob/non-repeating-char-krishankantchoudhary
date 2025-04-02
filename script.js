a46cefb6238999d3b48d6520170a4d2c64cce1c3
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-04-02 15:52:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	const charCount = new Map();
+
+	//count of each occirenece
+	for(let char of str){
+		charCount.set(char,(charCount.get(char)||0)+1);
+	}
+	for(let char of str){
+		if(charCount.get(char)==1){
+			return char;
+		}
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+// const input = prompt("Enter a string");
+// alert(firstNonRepeatedChar(input)); 
